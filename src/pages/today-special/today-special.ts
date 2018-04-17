02a11b449e32e8adaf1caa8555840481f122c3b0
satheeshrvs <satheeshkumar.mk@rapidvaluesolutions.com>
2018-04-18 00:14:08 +0530

    Menu page dependencies

--- src/pages/today-special/today-special.ts
+++ src/pages/today-special/today-special.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import {MenuPage} from '../menu/menu';
 
 /**
  * Generated class for the TodaySpecialPage page.
@@ -14,12 +15,17 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
   templateUrl: 'today-special.html',
 })
 export class TodaySpecialPage {
+  menuPage:any
   
   constructor(public navCtrl: NavController, public navParams: NavParams) {
   }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad TodaySpecialPage');
+  }
+
+  showMenuDetails(){
+    this.menuPage=MenuPage;
   }
 
 }
